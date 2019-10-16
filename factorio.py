15b496b514f8db024ea4aed3cdacf1ba62e7f46e
Nick Henderson <nhenderson019@gmail.com>
2019-10-16 05:27:49 +0000

    Update factorio.py

---
+++ factorio.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+import json
+
+class Item(object):
+    def __init__(self):
+        self.materials = []
+        self.raw_materials = []
+
+    def get_raw_materials(self):
+        return self.__get_raw_materials(-1, [], [], 0)
+
+    def __get_raw_materials(self, level, raw_materials_list, all_materials_list, time):
+        level += 1
+        for mat in self.materials:
+            if type(mat) == str:
+                raw_materials_list.append(mat)
+            elif type(mat) == int:
+                time += mat
+            else:
+                all_materials_list.append('|--' * level + mat.__class__.__name__)
+                mat.__get_raw_materials(level, raw_materials_list, all_materials_list, time)
+        return raw_materials_list, all_materials_list, time
+
+class CopperOre(Item):
+    def __init__(self):
+        super().__init__()
+        self.materials = ['CopperOre']
+
+class IronOre(Item):
+    def __init__(self):
+        super().__init__()
+        self.materials = ['IronOre']
+
+class CopperPlate(Item):
+    def __init__(self):
+        super().__init__()
+        self.materials = [CopperOre()]
+
+class IronPlate(Item):
+    def __init__(self):
+        super().__init__()
+        self.materials = [IronOre()]
+
+class IronGearWheel(Item):
+    def __init__(self):
+        super().__init__()
+        self.materials = [IronPlate(), IronPlate()]
+
+class AutomationSciencePack(Item):
+    def __init__(self):
+        super().__init__()
+        self.materials = [IronGearWheel(), CopperPlate(), Time(5)]
+
+class Time(Item):
+    def __init__(self, seconds):
+        super().__init__()
+        self.materials = [seconds]
+
+if __name__ == '__main__':
+    science = AutomationSciencePack()
+    raw_materials = defaultdict(int)
+    all_materials = science.get_raw_materials()[1]
+    time = science.get_raw_materials()[2]
+    for i in science.get_raw_materials()[0]:
+        raw_materials[i] += 1
+
+    print(json.dumps(raw_materials, indent=4))
+    print(json.dumps(all_materials, indent=4))
+    for i in all_materials:
+        print(i)

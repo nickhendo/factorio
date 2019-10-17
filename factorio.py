7543ae082823bf5ba7d2ea93f63184cbebf4a1b4
Nick <nhenderson019@gmail.com>
2019-10-17 22:01:42 +1000

    Touch ups, spacing, and times can now be floats or integers.

--- factorio.py
+++ factorio.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import json
 
+
 class Item(object):
     def __init__(self):
         self.materials = []
@@ -17,43 +18,50 @@ class Item(object):
         for mat in self.materials:
             if type(mat) == str:
                 raw_materials_list.append(mat)
-            elif type(mat) == int:
-                raw_materials_list.append(mat)
+            elif type(mat) in [float, int]:
                 time += mat
             else:
                 all_materials_dict[level].append(str(mat))
-                raw_materials_list, all_materials_dict, time = mat.__get_raw_materials(level, raw_materials_list, all_materials_dict, time)
+                raw_materials_list, all_materials_dict, time = mat.__get_raw_materials(level, raw_materials_list,
+                                                                                       all_materials_dict, time)
         return raw_materials_list, all_materials_dict, time
+
 
 class CopperOre(Item):
     def __init__(self):
         super().__init__()
         self.materials = ['CopperOre']
 
+
 class IronOre(Item):
     def __init__(self):
         super().__init__()
         self.materials = ['IronOre']
 
+
 class CopperPlate(Item):
     def __init__(self):
         super().__init__()
-        self.materials = [CopperOre()]
+        self.materials = [CopperOre(), Time(3.2)]
+
 
 class IronPlate(Item):
     def __init__(self):
         super().__init__()
-        self.materials = [IronOre(), Time(3)]
+        self.materials = [IronOre(), Time(3.2)]
+
 
 class IronGearWheel(Item):
     def __init__(self):
         super().__init__()
-        self.materials = [IronPlate(), IronPlate(), Time(5)]
+        self.materials = [IronPlate(), IronPlate(), Time(0.5)]
+
 
 class AutomationSciencePack(Item):
     def __init__(self):
         super().__init__()
-        self.materials = [IronGearWheel(), CopperPlate(), Time(10)]
+        self.materials = [IronGearWheel(), CopperPlate(), Time(5)]
+
 
 class Time(Item):
     def __init__(self, seconds):
@@ -63,16 +71,17 @@ class Time(Item):
     def __str__(self):
         return f"{self.__class__.__name__}: {' '.join(map(str, self.materials))} seconds"
 
+
 if __name__ == '__main__':
     science = AutomationSciencePack()
     raw_materials = defaultdict(int)
     all_materials = science.get_raw_materials()[1]
-    time = science.get_raw_materials()[2]
+    total_time = science.get_raw_materials()[2]
     for i in science.get_raw_materials()[0]:
         raw_materials[i] += 1
 
     print(json.dumps(raw_materials, indent=4))
     print(json.dumps(all_materials, indent=4))
-    print(f'Total Time: {time} seconds')
+    print(f'Total Time: {total_time} seconds')
     # for i in all_materials:
     #     print(i)
